4c613211a8c96ec1e1007984f209f7b5f11c44a7
dylanmatthews <djmatthews93@gmail.com>
2023-10-07 12:48:28 -0600

    lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      setTimeRemaining(10);
+      onAnswered(false);
+      return;
+    }
+
+    const intervalId = setInterval(() => {
+      setTimeRemaining((prev) => (prev > 0 ? prev - 1 : prev));
+    }, 1000);
+    return () => {
+      clearTimeout(intervalId);
+    };
+  }, [timeRemaining, onAnswered]);
+
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
